85cfce32ca39a00ec425b2e8b66e5fea38516a1e
Dragan Jovanović <drjovanovic@gmail.com>
2021-12-07 23:50:13 +0100

    bare projects

---
+++ TextParsingProject/TextParsingClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextParsingClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
